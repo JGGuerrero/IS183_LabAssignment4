47dc0fe0f0b81e5df182bd61311366be546db59a
JGGuerrero <jgguerrero@mail.fresnostate.edu>
2020-03-07 11:47:44 -0800

    first commit

---
+++ src/app/routes.ts
@@ -0,0 +1,44 @@
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ArtistComponent } from './artist/artist.component';
+import { ArtworkComponent } from './artwork/artwork.component';
+
+
+const Routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        resolve: {},
+        data: {
+            name: 'Home'
+        }
+    },
+    {
+        path: 'about',
+        component: AboutComponent,
+        resolve: {},
+        data: {
+            name: 'About'
+        }
+    },
+    {
+        path: 'artist',
+        component: ArtistComponent,
+        resolve: {},
+        data: {
+            name: 'Artist'
+        }
+    },
+    {
+        path: 'artworks',
+        component: ArtworkComponent,
+        resolve: {},
+        data: {
+            name: 'Art Works'
+        }
+    }
+];
+
+export const routerModule = RouterModule.forRoot(Routes);
+
